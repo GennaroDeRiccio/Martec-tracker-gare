e034d7510faecab989b6d0e7d47dfb6ac2f45147
Gennaro De Riccio <dericcio17@gmail.com>
2026-06-08 09:12:33 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ window.MARTEC_CONFIG = window.MARTEC_CONFIG || {
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvbWdpbWZqaXdrenlld3RmaXVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyNzAyMDIsImV4cCI6MjA4OTg0NjIwMn0.sfZhO7q0nUeMClAzcHnuFzUHNhg7hRWcThaeqOEP40k',
   syncTable: 'app_state',
   syncRowId: 'shared',
-  pollIntervalMs: 15000
+  pollIntervalMs: 0
 };
